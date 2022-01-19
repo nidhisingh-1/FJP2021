a9dceac7b42c04214fa216df9308f79f373228ee
Nidhi Singh <nidhi232singh@gmail.com>
2022-01-20 01:11:45 +0530

    recursion backtracking

--- src/Recursion/printEncoding.java
+++ src/Recursion/printEncoding.java
@@ -8,25 +8,37 @@ public class printEncoding {
 
         Scanner scn = new Scanner(System.in);
         String str = scn.nextLine();
-        printEncodings(str);
+        printEncodings(str, "");
 
     }
 
-    public static void printEncodings(String str) {
-
-        String ans = "";
+    public static void printEncodings(String str, String asf) {
 
         if(str.length() == 0){
-            System.out.println(ans);
+            System.out.println(asf);
             return;
         }
 
-        if(str.charAt(0) == 0){
+        char ch = str.charAt(0);
+        if(ch == '0'){
             return;
         }
 
+        String ros = str.substring(1);
+        int n = ch - '0';
+        char tobeadded = (char) (ch + n - 1);
+        printEncodings(ros , asf + tobeadded);
 
+        if(str.length() > 1){
+           char ch2 = str.charAt(1);
+           int num = n*10 + (ch2 - '0');
 
+           if(num <= 26){
+               char tobeadded2 = (char) ('a'+ num -1);
+               printEncodings(str.substring(2), asf + tobeadded2);
+           }
+
+        }
 
     }
 }
